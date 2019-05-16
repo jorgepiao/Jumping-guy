21d84522e0ddec9f58cfab3cc016056020843415
jorgepiao <piaosin11@gmail.com>
2019-05-16 17:56:32 -0500

    Iniciar el juego al presionar una tecla o boton
    -Crear varios estados de juego
    -Crear una UI con titulo y descripcion animada
    -Controlar el efecto Parallax y esconder UI

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -9,6 +9,10 @@ public class GameController : MonoBehaviour
     public float parallaxSpeed = 0.02f;
     public RawImage background;
     public RawImage platform;
+    public GameObject uiIdle;
+
+    public enum GameState {Idle, Playing};
+    public GameState gameState = GameState.Idle;
 
     // Start is called before the first frame update
     void Start()
@@ -19,9 +23,23 @@ public class GameController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float finalSpeed = parallaxSpeed * Time.deltaTime;
-        background.uvRect = new Rect(background.uvRect.x + finalSpeed, 0f, 1f, 1f);
-        platform.uvRect = new Rect(platform.uvRect.x + finalSpeed * 4, 0f, 1f, 1f);
+        // Empieza el juego
+        if (gameState == GameState.Idle && (Input.GetKeyDown("up") || Input.GetMouseButtonDown(0))){
+            gameState = GameState.Playing;
+            uiIdle.SetActive(false);
+        }
+
+        // Juego en marcha
+        else if (gameState == GameState.Playing){
+            Parallax();
+        }
+
+        void Parallax(){
+            float finalSpeed = parallaxSpeed * Time.deltaTime;
+            background.uvRect = new Rect(background.uvRect.x + finalSpeed, 0f, 1f, 1f);
+            platform.uvRect = new Rect(platform.uvRect.x + finalSpeed * 4, 0f, 1f, 1f);
+        }
+        
 
     }
 }
