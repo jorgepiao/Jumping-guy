0b395972fabac9c10e0d6b56810addb8640ba776
jorgepiao <piaosin11@gmail.com>
2019-05-20 03:49:25 -0500

    Musica y sonidos.. configuracion de la musica y los efectos de sonido del juego
    -Crear un AudioSource para reproducir la melodia
    -Crear otro AudioSource pra reproducir los efectos
    -Gestionar todo el audio desde scripts

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,22 +6,32 @@ public class PlayerController : MonoBehaviour
 {
     public GameObject game;
     public GameObject enemyGenerator;
+    public AudioClip jumpClip;
+    public AudioClip dieClip;
 
     private Animator animator;
+    private AudioSource audioPlayer;
+    private float startY;
 
     // Start is called before the first frame update
     void Start()
     {
         animator = GetComponent<Animator>();
+        audioPlayer = GetComponent<AudioSource>();
+        startY = transform.position.y;
     }
 
     // Update is called once per frame
     void Update()
     {
+        bool isGrounded = transform.position.y == startY;
         bool gamePlaying = game.GetComponent<GameController>().gameState == GameState.Playing;
+        bool userAction = Input.GetKeyDown("up") || Input.GetMouseButtonDown(0);
 
-        if (gamePlaying && (Input.GetKeyDown("up") || Input.GetMouseButtonDown(0))){
+        if (isGrounded && gamePlaying && userAction){
             UpdateState("PlayerJump");
+            audioPlayer.clip = jumpClip;
+            audioPlayer.Play();
         }
     }
 
@@ -36,6 +46,10 @@ public class PlayerController : MonoBehaviour
             UpdateState("PlayerDie");
             game.GetComponent<GameController>().gameState = GameState.Ended;
             enemyGenerator.SendMessage("CancelGenerator", true);
+
+            game.GetComponent<AudioSource>().Stop();
+            audioPlayer.clip = dieClip;
+            audioPlayer.Play();
         }
     }
 
