cb585a97bcc02850fd05846d8f1c5d7c3bb99b49
jorgepiao <piaosin11@gmail.com>
2019-05-22 02:55:54 -0500

    Cambio de player y background

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -12,6 +12,7 @@ public class GameController : MonoBehaviour
     public float parallaxSpeed = 0.02f;
     public RawImage background;
     public RawImage platform;
+    // public RawImage sun;
     public GameObject uiIdle;
 
     public GameState gameState = GameState.Idle;
@@ -61,6 +62,7 @@ public class GameController : MonoBehaviour
     void Parallax(){
         float finalSpeed = parallaxSpeed * Time.deltaTime;
         background.uvRect = new Rect(background.uvRect.x + finalSpeed, 0f, 1f, 1f);
+        // sun.uvRect = new Rect(sun.uvRect.x + finalSpeed + -2, 0f, 1f, 1f);
         platform.uvRect = new Rect(platform.uvRect.x + finalSpeed * 4, 0f, 1f, 1f);
     }
 
